81bc121501eb52e688ee546bbd805ccad10b687e
Rikard Sandström <rsandstroem@kpmg.com>
2017-01-16 17:12:19 +0100

    Add askName dialog as separate file

---
+++ messages/dialogs/askName.js
@@ -0,0 +1,29 @@
+var builder = require("botbuilder");
+var botbuilder_azure = require("botbuilder-azure");
+
+module.exports = {
+    Label: 'Ask name',
+    Dialog: [
+        function (session, args,  next) {
+            var user = builder.EntityRecognizer.findEntity(args.entities, 'User');
+            if (user) { // utterance contained a name
+                session.privateConversationData.username = user.entity;
+            }
+            if (!session.privateConversationData.username) { // Does not have name from earlier in the conversation or through the LUIS utterance
+                    builder.Prompts.text(session, 'Wie ist Ihre Name?');
+            } else {
+                session.endDialog();
+            }
+        },
+        function (session, results, next) {
+            if (results.response) {
+                session.privateConversationData.username = results.response;
+                next();
+            }
+        },
+        function (session) {
+            session.send("Hallo %s, es freut mich dass Sie den USR III Chatbot nutzen.", session.privateConversationData.username);
+            session.endDialog("Was möcten Sie im Zusammenhang mit der USR III wissen?");
+        }
+    ]
+}
