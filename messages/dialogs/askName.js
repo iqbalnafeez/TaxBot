af857f90c8c40e91b86059c9225dd7336b7e0e0a
Rikard Sandström <rsandstroem@kpmg.com>
2017-01-17 18:22:12 +0100

    Mimic contactForm style for askName

--- messages/dialogs/askName.js
+++ messages/dialogs/askName.js
@@ -4,21 +4,17 @@ var botbuilder_azure = require("botbuilder-azure");
 module.exports = {
     Label: 'Ask name',
     Dialog: [
-
         function (session) {
-            // Request user name
-            builder.Prompts.text(session, 'Wie ist Ihre Name?');
+            builder.Prompts.text(session, 'Wie lautet Ihre Name?');
         },
         function (session, results, next) {
             if (results.response) {
-                session.send("Have response");
                 session.privateConversationData.username = results.response;
                 next();
             }
         },
         function (session) {
-            session.send("Hallo %s, es freut mich dass Sie den USR III Chatbot nutzen.", session.privateConversationData.username);
-            session.endDialog("Was möcten Sie im Zusammenhang mit der USR III wissen?");
+            session.endDialog("Hallo %s, es freut mich dass Sie den USR III Chatbot nutzen.", session.privateConversationData.username);
         }
     ]
 }
