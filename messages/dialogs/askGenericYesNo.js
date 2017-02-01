a2f73f67dbbbddbf1cfbd604229552828e47fe90
igorzhilin <igor.zhilin@gmail.com>
2017-02-01 21:10:38 +0100

    fix case

--- messages/dialogs/askGenericYesNo.js
+++ messages/dialogs/askGenericYesNo.js
@@ -5,7 +5,8 @@ module.exports = {
     Label: 'Ask Generic Yes No',
     Dialog: [
         function (session, args) {
-            if (session.privateConversationData.usr3questions[session.privateConversationData.currentQuestionKey] !== '') {
+            // why usr3Questions currentQuestionKey should be empty??????
+            if (session.privateConversationData.usr3Questions[session.privateConversationData.currentQuestionKey] !== '') {
                 session.endDialog();
             } else {
                 builder.Prompts.choice(session, args.prompt, "Ja|Nein", {listStyle: 3, retryPrompt: "I verstehe nicht. Bitte antworten 'ja' oder 'nein'."});
@@ -13,7 +14,7 @@ module.exports = {
         },  
         function (session, results) {
             if (results.response) {
-                session.privateConversationData.usr3questions[session.privateConversationData.currentQuestionKey] = results.response.entity == "Ja";
+                session.privateConversationData.usr3Questions[session.privateConversationData.currentQuestionKey] = results.response.entity == "Ja";
                 session.endDialog();
             }
         }
