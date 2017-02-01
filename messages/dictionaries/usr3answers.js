d42aa40c12a49440dba380dbeb177786147cb0e5
igorzhilin <igor.zhilin@gmail.com>
2017-02-01 21:10:56 +0100

    new answers

--- messages/dictionaries/usr3answers.js
+++ messages/dictionaries/usr3answers.js
@@ -1,11 +1,38 @@
-module.exports = {
-    ordSteuersaetze: "Ord. Steuersätze heute & zukünftig",
-    auswirkungLatenteSteuern: "Auswirkung auf latente Steuern",
-    patentbox: "Patentbox",
-    altrechtlicherStepup: "altrechtlicherStepup",
-    neurechtlicherStepup: "neurechtlicherStepup",
-    feMehrfachabzug: "feMehrfachabzug",
-    nidSpecial: "NID mit spez. Margenapproach",
-    nidCommon: "Allg. NID",
-    erleichtungKapitalsteuer: "Erleichtung Kapitalsteuer"
+﻿module.exports = {
+	ordSteuersaetze: {
+		longText: "Möchten Sie wissen, wie sich die aktuellen ordentlichen Steuersätze in den Kantonen voraussichtlich ändern?",
+		glossary: ["Steuersatzsenkung HL"]
+	},
+	auswirkungLatenteSteuern: {
+		longText: "Die USR III könnte Auswirkungen auf die Beurteilung der latenten Steuern Ihres Unternehmens haben. Diese Auswirkungen stehen in engem Zusammenhang zum Enactment (finaler Beschluss) der USR III. Möchten Sie weitere Informationen zum Thema Enactment erhalten?",
+		glossary: ["Enactment"]
+	},
+	patentbox: {
+		longText: "Gemäss Ihren Antworten könnte die Patentbox für Sie von Interesse sein. Möchten Sie weitere Informationen zur Patentbox erhalten?",
+		glossary: ["Patentbox"]
+	},
+	altrechtlicherStepup: {
+		longText: "Gemäss Ihren Antworten könnte der altrechtliche Step up für Sie von Interesse sein. Möchten Sie weitere Informationen zum altrechtlichen Step up erhalten?",
+		glossary: ["Altrechtlicher Step up"]
+	},
+	neurechtlicherStepup: {
+		longText: "Gemäss Ihren Antworten könnte der neurechtliche Step up für Sie von Interesse sein. Möchten Sie weitere Informationen zum neurechtlichen Step up erhalten? Sie können dann später auch noch nach Informationen zum altrechtlichen Step up anfragen. Auch dieser könnte für Sie interessant sein.",
+		glossary: ["Neurechtlicher Step up"]
+	},
+	feMehrfachabzug: {
+		longText: "Gemäss Ihren Antworten könnte der F&E-Mehrfachabzug für Sie von Interesse sein. Möchten Sie weitere Informationen zum F&E-Mehrfachabzug erhalten?",
+		glossary: ["F&E Mehrfachabzug"]
+	},
+	nidCommon: {
+		longText: "Gemäss Ihren Antworten könnte der kalkulatorische Zinsabzug für Sie von Interesse sein. Möchten Sie weitere Informationen zum kalkulatorischen Zinsabzug erhalten?",
+		glossary: ["Kalkulatorischer Zinsabzug"]
+	},
+	nidSpecial: {
+		longText: "Gemäss Ihren Antworten könnte der kalkulatorische Zinsabzug für Sie von Interesse sein. Möchten Sie weitere Informationen zum kalkulatorischen Zinsabzug erhalten?",
+		glossary: ["Kalkulatorischer Zinsabzug"]
+	},
+	erleichtungKapitalsteuer: {
+		longText: "Gemäss Ihren Antworten könnte die Erleichterung der Kapitalsteuer für Sie von Interesse sein. Möchten Sie weitere Informationen zur Erleichterung der Kapitalsteuer erhalten? ",
+		glossary: ["Erleichterung Kapitalsteuer"]
+	}
 }
