552dd0ebfb27d0d282ea8350345b2d05536cffdd
igorzhilin <igor.zhilin@gmail.com>
2017-02-03 08:22:34 +0100

    wording

--- messages/dialogs/askName.js
+++ messages/dialogs/askName.js
@@ -10,7 +10,7 @@ module.exports = {
         function (session, results) {
             if (results.response) {
                 session.privateConversationData.username = results.response;
-                session.endDialog("Hallo %s, es freut mich dass Sie den USR III Chatbot nutzen. Bitte fragen sie mich 'was ist neue steuerreform' oder typpen 'auswirkungen' ein, um zu sehen, was für Auswirkungen die Reform auf Ihre Unternehmen haben kann.", session.privateConversationData.username);
+                session.endDialog("Hallo %s, es freut mich dass Sie den USR III Chatbot nutzen.\n\n\n\nBitte fragen sie mich 'was ist neue steuerreform' oder typpen 'auswirkungen' ein, um zu sehen, was für Auswirkungen die Reform auf Ihre Unternehmen haben kann.", session.privateConversationData.username);
             }
         }
     ]
